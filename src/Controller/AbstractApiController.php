a685db4a3a3ba0032e98477b955420239bc04dc3
Marek Mihalko <marek.mihalko@gmail.com>
2022-04-23 22:32:13 +0200

    Fix: Change api to FOSRestBundle

---
+++ src/Controller/AbstractApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use FOS\RestBundle\Context\Context;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+abstract class AbstractApiController extends AbstractFOSRestController
+{
+    protected function buildForm(string $type, $data = null, array $options = []): FormInterface
+    {
+        return $this->container->get('form.factory')->createNamed('', $type, $data, $options);
+    }
+
+    protected function respond($data, int $statusCode = Response::HTTP_OK, array $serializerGroups = []): Response
+    {
+        $view = $this->view($data, $statusCode);
+        $context = new Context();
+        $context->setGroups($serializerGroups);
+        $view->setContext($context);
+
+        return $this->handleView($view);
+    }
+}
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,46 +4,38 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Form\ProductType;
-use App\Service\FormErrorProcess;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProductController extends AbstractController
+class ProductController extends AbstractApiController
 {
     /**
      * @Route("/products", name="create_product", methods={"POST"})
      */
-    public function crateProduct(Request $request, EntityManagerInterface $entityManager, FormErrorProcess $formErrorProcess): Response
+    public function crateProduct(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $formData = json_decode($request->getContent(), true);
-
-        $form = $this->createForm(ProductType::class);
+        $form = $this->buildForm(ProductType::class);
         $form->handleRequest($request);
-        $form->submit($formData);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             /** @var Product $product */
             $product = $form->getData();
 
             $entityManager->persist($product);
             $entityManager->flush();
 
-            return $this->json($product, Response::HTTP_CREATED, [], ['groups' => ['product']]);
+            return $this->respond($product, Response::HTTP_OK, ['product']);
         }
 
-        return $this->json([
-            'message' => 'There was a validation error',
-            'errors' => $formErrorProcess->getErrorsFromForm($form)
-        ], Response::HTTP_BAD_REQUEST);
+        return $this->respond($form, Response::HTTP_BAD_REQUEST);
     }
 
     /**
      * @Route("/products/{id}", name="edit_product", methods={"PATCH", "PUT"})
      */
-    public function editProduct($id, Request $request, EntityManagerInterface $entityManager, FormErrorProcess $formErrorProcess): Response
+    public function editProduct($id, Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = $entityManager->getRepository(Product::class)->find($id);
 
@@ -62,13 +54,10 @@ class ProductController extends AbstractController
         if ($form->isValid()) {
             $entityManager->flush();
 
-            return $this->json($product, Response::HTTP_CREATED, [], ['groups' => ['product']]);
+            return $this->respond($product, Response::HTTP_OK, ['product']);
         }
 
-        return $this->json([
-            'message' => 'There was a validation error',
-            'errors' => $formErrorProcess->getErrorsFromForm($form)
-        ], Response::HTTP_BAD_REQUEST);
+        return $this->respond($form, Response::HTTP_BAD_REQUEST);
     }
 
     /**
@@ -79,12 +68,10 @@ class ProductController extends AbstractController
         $product = $entityManager->getRepository(Product::class)->find($id);
 
         if ($product) {
-            return $this->json($product, Response::HTTP_OK, [], ['groups' => ['product']]);
+            return $this->respond($product, Response::HTTP_OK, ['product']);
         }
 
-        return $this->json([
-            'message' => 'Product not exist',
-        ], Response::HTTP_BAD_REQUEST);
+        return $this->respond(['title' => 'Product not exist'], Response::HTTP_NOT_FOUND);
     }
 
     /**
@@ -97,12 +84,9 @@ class ProductController extends AbstractController
         if ($product) {
             $entityManager->remove($product);
             $entityManager->flush();
-
-            return $this->json([], Response::HTTP_NO_CONTENT);
+            return $this->respond(null,Response::HTTP_NO_CONTENT);
         }
 
-        return $this->json([
-            'message' => 'Product not exist',
-        ], Response::HTTP_NOT_FOUND);
+        return $this->respond(['title' => 'Product not exist'], Response::HTTP_NOT_FOUND);
     }
 }
