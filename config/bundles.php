a50a6e3b2640e7136755a75cd6df5dc0757180de
Marek Mihalko <marek.mihalko@gmail.com>
2022-04-24 17:23:57 +0200

    Build: Add FOSElasticaBundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    FOS\ElasticaBundle\FOSElasticaBundle::class => ['all' => true],
 ];
